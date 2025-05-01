6a011f9ad21a791c75bc53ccf6100c5ba97d8bf6
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-05-01 12:39:20 -0300

    Reorganizando os atributos da classe ContaBancaria para manter a ordem dos parâmetros do construtor

--- conta_banc.h
+++ conta_banc.h
@@ -7,16 +7,23 @@
 class ContaBancaria {
     private:
     int numero;
-    double saldo;
     Cliente titular;
+    double saldo;
+
 
     public:
     // Declaração do construtor parametrizado
     ContaBancaria(int numero, Cliente titular, double saldo = 0.0);
+
+    // Declaração dos métodos de "ações" que o usuário pode realizar
     void depositar(double valor);
     void sacar(double valor);
+
+    // Sobrecarga do método "transferir"
     void transferir(double valor, ContaBancaria &destino);
     void transferir(double valor, ContaBancaria &destino1, ContaBancaria &destino2);
+
+    // Métodos de visualização
     void exibirSaldo();
     void exibirInformacoes();
 
