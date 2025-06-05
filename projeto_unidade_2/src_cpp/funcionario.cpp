e7bf0c7a63a7128373954424994d8e07c5c299fa
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-06-05 20:57:55 -0300

    Implementando métodos da classe Funcionario

--- projeto_unidade_2/src_cpp/funcionario.cpp
+++ projeto_unidade_2/src_cpp/funcionario.cpp
@@ -1,1 +1,22 @@
 #include "funcionario.hpp"
+// #include <string>
+using namespace std;
+
+// Implementação do construtor para a inicialização da classe Funcionario
+Funcionario::Funcionario(string nome, float salarioBase, int id):
+    nome(nome), salarioBase(salarioBase), id(id) {/* vazio */}
+
+// Métedo get para acessar e "pegar/capturar" o atributo nome da classe Funcionario
+string Funcionario::getNome() {
+    return nome;
+}
+
+// Métedo get para acessar e "pegar/capturar" o atributo SalarioBase da classe Funcionario
+float Funcionario::getSalarioBase() {
+    return salarioBase;
+}
+
+// Métedo get para acessar e "pegar/capturar" o atributo id da classe Funcionario
+int Funcionario::getId() {
+    return id;
+}
