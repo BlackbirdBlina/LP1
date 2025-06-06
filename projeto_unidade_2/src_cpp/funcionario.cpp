6e5a25ce19be618e95646355bf3746a1a935ba71
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-06-06 17:04:36 -0300

    Adicionando a implementação dos métodos setters e atualizando comentários

--- projeto_unidade_2/src_cpp/funcionario.cpp
+++ projeto_unidade_2/src_cpp/funcionario.cpp
@@ -1,22 +1,32 @@
 #include "funcionario.hpp"
-// #include <string>
 using namespace std;
 
 // Implementação do construtor para a inicialização da classe Funcionario
 Funcionario::Funcionario(string nome, float salarioBase, int id):
-    nome(nome), salarioBase(salarioBase), id(id) {/* vazio */}
+    nome(nome), salarioBase(salarioBase), id(id) {}
 
-// Métedo get para acessar e "pegar/capturar" o atributo nome da classe Funcionario
+// Métodos setters para acessar e modificar os aributos nome, salarioBase e id da classe Funcionario
+void Funcionario::setNome(string nome) {
+    this->nome = nome;
+}
+
+void Funcionario::setSalarioBase(float salarioBase) {
+    this->salarioBase = salarioBase;
+}
+
+void Funcionario::setId(int id) {
+    this->id = id;
+}
+
+// Métedos getters para acessar e "pegar/capturar" os atributos nome, salarioBase e id da classe Funcionario
 string Funcionario::getNome() {
     return nome;
 }
 
-// Métedo get para acessar e "pegar/capturar" o atributo SalarioBase da classe Funcionario
 float Funcionario::getSalarioBase() {
     return salarioBase;
 }
 
-// Métedo get para acessar e "pegar/capturar" o atributo id da classe Funcionario
 int Funcionario::getId() {
     return id;
 }
